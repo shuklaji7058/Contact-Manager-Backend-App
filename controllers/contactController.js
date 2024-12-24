84b98024ce4ceff8f34630056a2c58aa23b4fac1
Anish Shukla <shuklaanish040@gmail.com>
2024-12-24 18:37:38 +0530

    node

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -18,7 +18,13 @@ const createContact = asyncHandler(async (req, res) => {
     res.status(400);
     throw new Error("All fields are mandatory !");
   }
-  res.status(201).json({ message: "Create contacts" });
+
+  const contact = await Contact.create({
+    name,
+    email,
+    phone,
+  });
+  res.status(201).json(contact);
 });
 
 //@desc Get contacts
