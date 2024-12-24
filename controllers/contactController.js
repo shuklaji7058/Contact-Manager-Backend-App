c21486bd8bdf747daa1c0ccb504875cdc09ecd16
Anish Shukla <shuklaanish040@gmail.com>
2024-12-24 19:07:06 +0530

    node

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -61,7 +61,13 @@ const updateContact = asyncHandler(async (req, res) => {
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact = asyncHandler(async (req, res) => {
-  res.status(201).json({ message: `Delete contact for ${req.params.id}` });
+  const contact = await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("contact not found");
+  }
+  await Contact.deleteOne();
+  res.status(200).json(contact);
 });
 
 module.exports = {
